89e4b42400fdfab456b8b8568cac8d1abc67767f
Shreya_Kashyap <kashyapshreya13@gmail.com>
2022-06-02 22:45:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const natsu = document.getElementById("natsu");
+
+function jump(){
+  if(natsu.classlist != "jump"){
+   natsu.classList.add("jump");
+
+   setTimeout(function() {
+     natsu.classList.remove("jump");
+   },300);
+}
+}
+
+document.addEventListener("keydown", function(event){
+  jump();
+});
